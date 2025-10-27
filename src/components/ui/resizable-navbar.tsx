104d6ab5e3898159dfaeb357413038d2e4958221
gustavoch <gustavo.francelino@gmail.com>
2025-10-27 20:53:09 -0300

    hero

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -211,10 +211,7 @@ export const NavbarLogo = () => {
       href="#"
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
-      <img src="/avatar.jpg" className="rounded-full" alt="logo" width={30} height={30} />
-      <span className="font-medium text-black dark:text-white">
-        Gustavo
-      </span>
+      <img src="/avatar.jpg" className="rounded-full" alt="logo" width={40} height={40} />
     </a>
   )
 }
